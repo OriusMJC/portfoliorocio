b6d5c1786ab99284b2d313460621e325d56539d2
OriusMJC <matiasjesuscontreras12@hotmail.com>
2022-01-27 18:50:39 -0300

    Iniciando portfolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <section id='section1'>
+
+      </section>
+      <section id='section2'>
+
+      </section>
+      <section id='section3'>
+
+      </section>
+    </Router>
+  );
+}
+
+export default App;
